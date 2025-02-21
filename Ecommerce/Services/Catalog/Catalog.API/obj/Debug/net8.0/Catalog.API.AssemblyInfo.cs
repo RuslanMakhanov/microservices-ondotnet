c3c065b8c79622c8e37d1a3c6c5c1cf059bf3274
RuslanMakhanov <misterruslan1701@gmail.com>
2025-02-21 17:22:40 +0500

    Created new handlers and maps

--- Ecommerce/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ Ecommerce/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd61151eac84256d44f3ff603471a469b806ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88228feaa857acfa21a86b121689d453bb2d1623")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Ecommerce/Services/Catalog/Catalog.Application/Queries/GetProductByBrandQuery.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByBrandQuery : IRequest<List<ProductResponse>>
+    {
+        public string BrandName { get; set; }
+        public GetProductByBrandQuery(string brandName)
+        {
+            BrandName = brandName;
+        }
+
+    }
+}
